75fb70a57981c71fbd32cd1e8a05c0ecaef58ef0
Paolo Pelesk <ppelesk@gmail.com>
2024-12-02 20:49:44 +0100

    Solution for part 1 and two

--- 2/2.php
+++ 2/2.php
@@ -37,6 +37,37 @@ foreach ($data as $report) {
 
 echo "Number of safe reports: " . $safeCount . "\n";
 
+/**
+ * --- Day 2: ---
+ */
+
+function dampener($report) {
+    $increasing = null; // null means not determined yet
+    for ($i = 0; $i < count($report) - 1; $i++) {
+        $diff = $report[$i + 1] - $report[$i];
+        if ($diff == 0 || abs($diff) > 3) {
+            print_r($report[$i]);
+        }
+        if ($increasing === null) {
+            $increasing = $diff > 0;
+        } else {
+            if (($increasing && $diff < 0) || (!$increasing && $diff > 0)) {
+                return false;
+            }
+        }
+    }
+  //  echo "Safe report: " . implode(" ", $report) . "\n";
+    return true;
+}
+
+$safeCount = 0;
+
+foreach ($data as $report) {
+    if (dampener($report)) {
+        $safeCount++;
+    }
+}
+
 
 
 
